0ad973b285f7a0252236217bbe31fe97fd1c58e3
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-10-09 15:45:27 -0700

    finished mvp

--- src/main/java/com/lambdaschool/orders/services/CustomerServiceImpl.java
+++ src/main/java/com/lambdaschool/orders/services/CustomerServiceImpl.java
@@ -1,11 +1,13 @@
 package com.lambdaschool.orders.services;
 
 import com.lambdaschool.orders.models.Customer;
+import com.lambdaschool.orders.models.Order;
 import com.lambdaschool.orders.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Transactional
@@ -18,7 +20,9 @@ public class CustomerServiceImpl implements CustomerService
     @Override
     public List<Customer> findall()
     {
-        return null;
+        List<Customer> rtnList = new ArrayList<>();
+        custrepos.findAll().iterator().forEachRemaining(rtnList::add);
+        return rtnList;
     }
 
     @Override
@@ -31,13 +35,27 @@ public class CustomerServiceImpl implements CustomerService
     @Override
     public Customer save(Customer customer)
     {
-        return null;
+        Customer newCustomer = new Customer();
+
+        newCustomer.setCustname(customer.getCustname());
+        newCustomer.setCustcity(customer.getCustcity());
+        newCustomer.setWorkingarea(customer.getWorkingarea());
+        newCustomer.setCustcountry(customer.getCustcountry());
+        newCustomer.setGrade(customer.getGrade());
+        newCustomer.setOpeningamt(customer.getOpeningamt());
+        newCustomer.setReceiveamt(customer.getReceiveamt());
+        newCustomer.setPaymentamt(customer.getPaymentamt());
+        newCustomer.setOutstandingamt(customer.getOutstandingamt());
+        newCustomer.setPhone(customer.getPhone());
+        newCustomer.setAgent(customer.getAgent());
+
+        return custrepos.save(newCustomer);
     }
 
     @Override
     public Customer update(Customer customer, long id)
     {
-        return null;
+        return custrepos.save(customer);
     }
 
     @Override
